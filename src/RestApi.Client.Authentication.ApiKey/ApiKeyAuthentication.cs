2f3fcfb304ee75e458e1dc1fcda3d8a40f96c084
Mihir Dilip <mihir_dilip@hotmail.co.uk>
2019-12-11 14:06:48 +0000

    Added Copyright

--- src/RestApi.Client.Authentication.ApiKey/ApiKeyAuthentication.cs
+++ src/RestApi.Client.Authentication.ApiKey/ApiKeyAuthentication.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) Mihir Dilip. All rights reserved.
+// Licensed under the MIT License. See License in the project root for license information.
+
+using System;
 
 namespace RestApi.Client.Authentication
 {
